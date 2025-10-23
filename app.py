031163d9d6309f06ac52b43438381a8d2858cc4e
Gwonhoemin <hoemin06@naver.com>
2025-10-23 18:06:55 +0900

    기본 데이터셋 구성

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+def main():
+    st.title('Seafood forecasting')
+
+    menu = ['Home', 'EDA', 'ML']
+    choice = st.sidebar.selectbox('메뉴', menu)
+
+    if choice == menu[0]:
+        pass
+    elif choice == menu[1]:
+        pass
+    elif choice == menu[2]:
+        pass
+
+    
+if __name__ == '__main__':
+    main()
