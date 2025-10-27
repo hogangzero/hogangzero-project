b6d6ac935a3940966617d6fd0f8c6b3788f14d97
devel-hj <k43354397@gmail.com>
2025-10-27 15:45:17 +0900

    app 초기 화면 커밋

--- app.py
+++ app.py
@@ -3,14 +3,26 @@ import streamlit as st
 def main():
     st.title('Seafood forecasting')
 
-    menu = ['Home', 'EDA', 'ML']
+    menu = ['Home', '경락가', 'ML', '정보']
+    sub_menu = ['어종별 경락가', '산지별 경락가', '상태별(활어, 선어, 냉동) 경락가']
+
     choice = st.sidebar.selectbox('메뉴', menu)
+    
 
     if choice == menu[0]:
-        pass
+        run_home()
     elif choice == menu[1]:
-        pass
+        sub_choice = st.sidebar.selectbox('경락가', sub_menu)
+        if sub_choice == sub_menu[0] :
+            species_price() # 어종별 경락가
+        elif sub_choice == sub_menu[1] :
+            source_price() # 산지별 경락가
+        elif sub_choice == sub_menu[2] :
+            status_price() # 상태별 경락가
+        
     elif choice == menu[2]:
+        pass
+    elif choice == menu[3]:
         pass
 
     
